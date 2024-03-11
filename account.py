4e8d3923e9e8b93951416966b3329a080ccbe6ab
dr33k <dr33k@users.noreply.github.com>
2024-03-11 12:47:08 +0100

    Created Account Class

--- account.py
+++ account.py
@@ -1,3 +1,19 @@
-print("Michael")
-print("11-03-2024")
-print("11-03-2024")
+class Account:
+
+    def __init__(self, fname, lname, phone_no, email, bvn, passcode, pin):
+        self.__fname = fname
+        self.__lname = lname
+        self.__phone_no = phone_no
+        self.__email = email
+        self.__pin = pin
+        self.__passcode = passcode
+        self.__address = None
+        self.__bvn = bvn
+        self.__balance = 0.0
+        # To be implemented later
+        # self.__account_no =
+        self.nin = None
+
+
+
+
